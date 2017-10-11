8ddcdf18f8cec33aada3af4ed5d46fa55a9a0911
Alexis Renault <renaultalexis35@gmail.com>
2017-10-11 12:14:28 +0200

    Class creation

---
+++ src/Display.java
@@ -0,0 +1,12 @@
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.paint.Color;
+import javafx.stage.Stage;
+
+public class Display extends Application {
+
+}
