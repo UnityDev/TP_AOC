6ef4d9f7ba4b35c49197d4826cabd4785294d059
Alexis Renault <renaultalexis35@gmail.com>
2017-10-24 15:05:49 +0200

    Commit 2 - class creation

--- src/Channel.java
+++ src/Channel.java
@@ -1,9 +1,19 @@
 
 
-public class Channel {
+public class Channel implements Generator, ObservatorGenerator{
 
     public void update(Generator s)
     {
+
+    }
+
+    @Override
+    public void attach(Observer o) {
+        
+    }
+
+    @Override
+    public void detach(Observer o) {
 
     }
 
